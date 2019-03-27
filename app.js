c786e5433080d378715032ec6a665750a3bbbec4
ygao0719 <47805485+ygao0719@users.noreply.github.com>
2019-03-26 20:27:34 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+
+var place = prompt('Am I in Seattle?');
+if (place.toLowerCase()==='y' || place.toLowerCase()==='yes'){
+  console.log('Yes. I am in Seattle')
+  alert('Great, you are right!');
+}else {
+  console.log('No. I am in Seattle')
+  alert ('Sorry, you are wrong!');
+}
+
+var degree = prompt('Do I have a degree on computer science?');
+if (degree.toLowerCase() === 'y' || degree.toLowerCase() ==='yes'){
+  console.log('yes. I do')
+  alert('Great, you are right!');
+}else {
+  console.log('No. I do')
+  alert ('Sorry, you are wrong!');
+}
+
+var pet= prompt('Do I like dog?');
+if (pet.toLowerCase() === 'y' || pet.toLowerCase() === 'yes'){
+  console.log('yes. I do')
+  alert('Great, you are right!');
+}else {
+  console.log('No. I do')
+  alert ('Sorry, you are wrong!');
+}
+
+var habit = prompt('Do I like swimming?');
+if (habit.toLowerCase() === 'y' || habit.toLowerCase() === 'yes'){
+  console.log('yes. I do')
+  alert('Great, you are right!');
+}else {
+  console.log('No. I do')
+  alert ('Sorry, you are wrong!');
+}
+
+var food = prompt('Do I like eating Chinese food?');
+if (food.toLowerCase() === 'y' || food.toLowerCase() === 'yes'){
+  console.log('yes. I do')
+  alert('Great, you are right!');
+}else {
+  console.log('No. I do')
+  alert ('Sorry, you are wrong!');
+}
+
+
